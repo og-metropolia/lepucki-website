c7da07dd9eff25c9d21759cf865daf12d3d0c375
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2023-04-24 13:43:30 +0300

    Gridi etenee

--- src/components/Gristesti.jsx
+++ src/components/Gristesti.jsx
@@ -2,28 +2,66 @@ import React from 'react';
 import './gridtesti.css';
 
 export default function Gridi() {
+  var curr = new Date(); // get current date
+  //var first = curr.getDate(); // - curr.getDay(); // First day is the day of the month - the day of the week
+  const day = curr.getDay();
+  var firstday = new Date(
+    new Date().getTime() - 60 * 60 * 24 * (day - 1) * 1000
+  ).toLocaleDateString();
+
   return (
     <div className="wrapper">
-      <div className="box" id="vkpaivat">
-        Maanantai
+      <div className="box">
+        <p id="vkpaivat">Maanantai</p>
+        <p id="paivamaara">{firstday}</p>
       </div>
-      <div className="box" id="vkpaivat">
-        Tiistai
+      <div className="box">
+        <p id="vkpaivat">Tiistai</p>
+        <p id="paivamaara">
+          {new Date(
+            new Date().getTime() + 60 * 60 * 24 * 1 * 1000
+          ).toLocaleDateString()}
+        </p>
       </div>
-      <div className="box" id="vkpaivat">
-        Keskiviikko
+      <div className="box">
+        <p id="vkpaivat">Keskiviikko</p>
+        <p id="paivamaara">
+          {new Date(
+            new Date().getTime() + 60 * 60 * 24 * 2 * 1000
+          ).toLocaleDateString()}
+        </p>
       </div>
-      <div className="box" id="vkpaivat">
-        Torstai
+      <div className="box">
+        <p id="vkpaivat">Torstai</p>
+        <p id="paivamaara">
+          {new Date(
+            new Date().getTime() + 60 * 60 * 24 * 3 * 1000
+          ).toLocaleDateString()}
+        </p>
       </div>
-      <div className="box" id="vkpaivat">
-        Perjantai
+      <div className="box">
+        <p id="vkpaivat">Perjantai</p>
+        <p id="paivamaara">
+          {new Date(
+            new Date().getTime() + 60 * 60 * 24 * 4 * 1000
+          ).toLocaleDateString()}
+        </p>
       </div>
-      <div className="box" id="vkpaivat">
-        Lauantai
+      <div className="box">
+        <p id="vkpaivat"> Lauantai</p>
+        <p id="paivamaara">
+          {new Date(
+            new Date().getTime() + 60 * 60 * 24 * 5 * 1000
+          ).toLocaleDateString()}
+        </p>
       </div>
-      <div className="box" id="vkpaivat">
-        Sunnuntai
+      <div className="box">
+        <p id="vkpaivat">Sunnuntai</p>
+        <p id="paivamaara">
+          {new Date(
+            new Date().getTime() + 60 * 60 * 24 * 6 * 1000
+          ).toLocaleDateString()}
+        </p>
       </div>
       {kellonAika()}
     </div>
@@ -32,40 +70,39 @@ export default function Gridi() {
 
 function kellonAika() {
   const aikavali = [];
-  let alkuaika = 4.0;
-  let loppuaika = 7.0;
 
-  for (let i = 1; i < 6; i++) {
-    alkuaika += 3;
-    loppuaika += 3;
-    const verti = (
-      <button className="kellonaikaButton">
+  let alkuaika = 4;
+  let loppuaika = 7;
+
+  for (let i = 0; i < 35; i++) {
+    if (i % 7 == 0) {
+      alkuaika += 3;
+      loppuaika += 3;
+    }
+    const napit = (
+      <button className="kellonaikaButton" name={i} onClick={valinta}>
         {alkuaika.toFixed(2)} - {loppuaika.toFixed(2)}
       </button>
     );
-    aikavali.push(verti);
 
-    for (let j = 1; j < 7; j++) {
-      const hori = (
-        <button className="kellonaikaButton">
-          {alkuaika.toFixed(2)} - {loppuaika.toFixed(2)}
-        </button>
-      );
-      aikavali.push(hori);
-    }
+    aikavali.push(napit);
   }
   return aikavali;
 }
 
-const napit = document.querySelectorAll('.kellonaikaButton');
+let varatut = [];
 
-napit.forEach((nappi) => {
-  nappi.addEventListener('click', () => {
-    nappi.classList.toggle('valittu');
+function valinta(event) {
+  const valittu = 'valittu';
+  const eiValittu = 'kellonaikaButton';
 
-    const valitutNapit = document.querySelectorAll('.kellonaikaButton.valittu');
-    valitutNapit.forEach((valittuNappi) => {
-      valittuNappi.classList.add('valittu');
-    });
-  });
-});
+  if (event.target.className === valittu) {
+    event.target.className = eiValittu;
+    varatut = varatut.filter((item) => item !== event.target.name);
+  } else {
+    event.target.className = valittu;
+    varatut.push(event.target.name);
+  }
+
+  console.log(varatut);
+}
